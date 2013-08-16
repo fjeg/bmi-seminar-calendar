e7d0380be084cf36b120113e4ec4840dfef6312d
Francisco Jose Gimenez <fgimenez@corn22.stanford.edu>
2013-08-16 00:00:25 -0700

    added cron script for stanford corn

---
+++ scripts/run_cron.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+#export PYTHONPATH="/afs/ir/users/f/g/fgimenez/.virtualenvs/bro_calendar/lib/python2.7/site-packages/"
+#/afs/ir.stanford.edu/users/f/g/fgimenez/.virtualenvs/bro_calendar/bin/python get_all_events.py
+source /afs/ir/users/f/g/fgimenez/.virtualenvs/bro_calendar/bin/activate
+python get_all_events.py
+
